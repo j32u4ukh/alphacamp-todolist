981dbaed46eec0d0a8a6c1cf8f966b0b23de7f67
j32u4ukh <j32u4ukh@gmail.com>
2024-01-07 20:56:22 +0800

    feat: 路由初始化

--- index.js
+++ index.js
@@ -12,6 +12,34 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.get("/todos", (req, res) => {
+  res.send("get all todos");
+});
+
+app.post("/todos", (req, res) => {
+  res.send("add todo");
+});
+
+app.get("/todos/:id", (req, res) => {
+  res.send(`get todo: ${req.params.id}`);
+});
+
+app.put("/todos/:id", (req, res) => {
+  res.send(`modify todo: ${req.params.id}`);
+});
+
+app.delete("/todos/:id", (req, res) => {
+  res.send(`delete todo: ${req.params.id}`);
+});
+
+app.get("/todos/:id/edit", (req, res) => {
+  res.send(`get todo edit: ${req.params.id}`);
+});
+
+app.get("/todos/new", (req, res) => {
+  res.send("create todo");
+});
+
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`);
 });
