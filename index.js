0477be15c0ed6c917149bf193ec4851e52673419
j32u4ukh <j32u4ukh@gmail.com>
2024-01-08 22:26:03 +0800

    feat: 新增 todo

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.set("view engine", ".hbs");
 app.set("views", "./views");
 app.use(express.static("public"));
 
+// 需要使用 express.urlencoded 來從請求網址中獲取表單資料，否則就會回傳 undefined
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.render("index");
 });
@@ -30,7 +33,19 @@ app.get("/todos", (req, res) => {
 });
 
 app.post("/todos", (req, res) => {
-  res.send("add todo");
+  const name = req.body.name;
+
+  return Todo.create({ name })
+    .then(() => {
+      res.redirect("/todos");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/todos/new", (req, res) => {
+  return res.render("new");
 });
 
 app.get("/todos/:id", (req, res) => {
@@ -47,10 +62,6 @@ app.delete("/todos/:id", (req, res) => {
 
 app.get("/todos/:id/edit", (req, res) => {
   res.send(`get todo edit: ${req.params.id}`);
-});
-
-app.get("/todos/new", (req, res) => {
-  res.send("create todo");
 });
 
 app.listen(port, () => {
