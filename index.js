fba9dbfbe639a124b8e69db619973ed48d61dd3a
j32u4ukh <j32u4ukh@gmail.com>
2024-01-08 22:28:24 +0800

    feat: 查看 todo 內容

--- index.js
+++ index.js
@@ -49,7 +49,18 @@ app.get("/todos/new", (req, res) => {
 });
 
 app.get("/todos/:id", (req, res) => {
-  res.send(`get todo: ${req.params.id}`);
+  const id = req.params.id;
+
+  return Todo.findByPk(id, {
+    attributes: ["id", "name"],
+    raw: true,
+  })
+    .then((todo) => {
+      res.render("todo", { todo });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.put("/todos/:id", (req, res) => {
