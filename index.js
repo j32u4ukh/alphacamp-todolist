60627f6fc076c9577132ce3a68927ac1e89f6a20
j32u4ukh <j32u4ukh@gmail.com>
2024-01-15 21:47:45 +0800

    feat: 使用 connect-flash 呈現一次性結果訊息

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const { engine } = require("express-handlebars");
 const methodOverride = require("method-override");
+const flash = require("connect-flash");
+const session = require("express-session");
+
 const app = express();
 const port = 3000;
 const db = require("./models");
@@ -18,6 +21,19 @@ app.use(express.urlencoded({ extended: true }));
 // 「覆寫 (override)」HTTP 方法，允許表單傳送 GET 和 POST 以外的方法
 app.use(methodOverride("_method"));
 
+// secret：為了保護 session 放到瀏覽器的 cookie 後不會被篡改與偽造，這個參數是 session 用來驗證 session id 的字串，這組字串會由伺服器設定，不會洩露給用戶端。
+// 下面兩個設定項沒有設置也能正常運行，只是會在 server 上跳出一些提示訊息，建議還是把它設定起來。
+// resave：當設定為 true 時，會在每一次與使用者互動後，強制把 session 更新到 session store 裡。
+// saveUninitialized：強制將未初始化的 session 存回 session store。未初始化表示這個 session 是新的而且沒有被修改過，例如未登入的使用者的 session。
+app.use(
+  session({
+    secret: "ThisIsSecret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(flash());
+
 app.get("/", (req, res) => {
   res.render("index");
 });
@@ -28,7 +44,7 @@ app.get("/todos", (req, res) => {
     raw: true,
   })
     .then((todos) => {
-      res.render("todos", { todos });
+      res.render("todos", { todos, message: req.flash("success") });
     })
     .catch((err) => {
       res.status(422).json(err);
@@ -40,6 +56,7 @@ app.post("/todos", (req, res) => {
 
   return Todo.create({ name })
     .then(() => {
+      req.flash("success", "新增成功!");
       res.redirect("/todos");
     })
     .catch((err) => {
@@ -78,6 +95,7 @@ app.put("/todos/:id", (req, res) => {
     },
     { where: { id } }
   ).then(() => {
+    req.flash("success", "更新成功!");
     res.redirect(`/todos/${id}`);
   });
 });
@@ -85,7 +103,10 @@ app.put("/todos/:id", (req, res) => {
 app.delete("/todos/:id", (req, res) => {
   const id = req.params.id;
 
-  return Todo.destroy({ where: { id } }).then(() => {
+  return Todo.destroy({
+    where: { id },
+  }).then(() => {
+    req.flash("success", "刪除成功!");
     res.redirect("/todos");
   });
 });
